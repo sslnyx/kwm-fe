c2e8f0f3f7f0f8c0dc09689aa865172f14943c67
sslnyx <sslnyx@gmail.com>
2025-12-20 00:04:26 -0800

    chore: Remove specific page build outputs and update Astro configuration and `.htaccess`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   trailingSlash: 'never',
+  build: {
+    format: 'file'
+  },
   vite: {
     plugins: [tailwindcss()]
   }
